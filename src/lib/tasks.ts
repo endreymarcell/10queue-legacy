c47d34187bd60038d826a2a730e883aac0d15961
Marcell Endrey <marcell.endrey@prezi.com>
2021-10-05 17:43:49 +0200

    simplify overengineered type even more

--- src/lib/tasks.ts
+++ src/lib/tasks.ts
@@ -1,13 +1,9 @@
-import type { uniqueTypeSymbol } from "./utils"
-
-let taskIdCounter = 0
 export type Task = {
-    id: number
     title: string
 }
 
 function createTask(title: string) {
-    return { id: taskIdCounter++, title }
+    return { title }
 }
 
 // TODO dummy data for the early phase of development
