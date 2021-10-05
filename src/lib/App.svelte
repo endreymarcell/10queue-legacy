54f6456c66c0f7a60a8666c088fda63e427ca2c7
Marcell Endrey <marcell.endrey@prezi.com>
2021-10-05 15:29:45 +0200

    also extract TaskList component

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -1,27 +1,7 @@
 <script lang="ts">
-    import TaskRow from "$lib/TaskRow.svelte"
-    import { derived } from "svelte/store"
     import Header from "./Header.svelte"
-    import { tasks } from "./state"
-
-    const paddedTasksList = derived(tasks, ($tasks) => [...$tasks, ...new Array(10 - $tasks.length)])
+    import TaskList from "./TaskList.svelte";
 </script>
 
 <Header />
-<div class="task-container">
-    {#each $paddedTasksList as task, index}
-        <TaskRow {task} {index} />
-    {/each}
-</div>
-
-<style>
-    .task-container {
-        --padding: 50px;
-        width: calc(100% - 2 * var(--padding));
-        height: calc(100% - 2 * var(--padding) - 100px);
-        padding: var(--padding);
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-    }
-</style>
+<TaskList />
---
+++ src/lib/TaskList.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import TaskRow from "$lib/TaskRow.svelte"
+    import { derived } from "svelte/store"
+    import { tasks } from "./state"
+
+    const paddedTasksList = derived(tasks, ($tasks) => [...$tasks, ...new Array(10 - $tasks.length)])
+</script>
+
+<div class="task-container">
+    {#each $paddedTasksList as task, index}
+        <TaskRow {task} {index} />
+    {/each}
+</div>
+
+<style>
+    .task-container {
+        --padding: 50px;
+        width: calc(100% - 2 * var(--padding));
+        height: calc(100% - 2 * var(--padding) - 100px);
+        padding: var(--padding);
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+</style>
