b300150675df5aeba9238f16b3aa831bf2d44aae
Marcell Endrey <marcell.endrey@prezi.com>
2021-10-05 17:43:00 +0200

    simplify overengineered type

--- src/lib/tasks.ts
+++ src/lib/tasks.ts
@@ -1,14 +1,18 @@
 import type { uniqueTypeSymbol } from "./utils"
 
-export type TaskId = number & { [uniqueTypeSymbol]: "TaskId" }
+let taskIdCounter = 0
 export type Task = {
-    id: TaskId
+    id: number
     title: string
+}
+
+function createTask(title: string) {
+    return { id: taskIdCounter++, title }
 }
 
 // TODO dummy data for the early phase of development
 export const defaultTasks: Task[] = [
-    { id: 0 as TaskId, title: "foo" },
-    { id: 1 as TaskId, title: "bar" },
-    { id: 2 as TaskId, title: "baz" },
+    createTask('foo'),
+    createTask('bar'),
+    createTask('baz'),
 ]
