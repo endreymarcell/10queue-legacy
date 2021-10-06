1dde0b554f12c985922dc59913ab72dd9a4c49c6
Marcell Endrey <marcell.endrey@prezi.com>
2021-10-06 09:17:50 +0200

    rename and comment

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -8,29 +8,30 @@ export type Events = {
     taskPaused: void
 }
 
+// It is safe to modify the state in the updaters because these functions are fed to immer
 export const logic: Logic = {
     taskTitleEdited: {
         action: createAction("taskTitleEdited", (index, title) => ({ index, title })),
-        updater: payload => draft => {
-            draft.tasks[payload.index].title = payload.title
+        updater: payload => state => {
+            state.tasks[payload.index].title = payload.title
         },
     },
     taskDeleted: {
         action: createAction("taskDeleted", index => ({ index })),
-        updater: payload => draft => {
-            draft.tasks.splice(payload.index, 1)
+        updater: payload => state => {
+            state.tasks.splice(payload.index, 1)
         },
     },
     taskStarted: {
         action: createAction("taskStarted"),
-        updater: () => draft => {
-            draft.isRunning = true
+        updater: () => state => {
+            state.isRunning = true
         },
     },
     taskPaused: {
         action: createAction("taskPaused"),
-        updater: () => draft => {
-            draft.isRunning = false
+        updater: () => state => {
+            state.isRunning = false
         },
     },
 }
