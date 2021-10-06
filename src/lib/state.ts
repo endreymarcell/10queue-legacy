056824185224f5701fedb013c322257d5754fda1
Marcell Endrey <marcell.endrey@prezi.com>
2021-10-06 08:52:55 +0200

    why is prettier still not running?

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -3,56 +3,62 @@ import { writable } from "svelte/store"
 import { defaultTasks } from "./tasks"
 import type { Task } from "./tasks"
 import type { WritableDraft } from "immer/dist/internal"
-import { ActionCreator, ActionCreatorWithPayload, ActionTypeFromActionCreators, createAction } from "redux-dry-ts-actions"
+import {
+    ActionCreator,
+    ActionCreatorWithPayload,
+    ActionTypeFromActionCreators,
+    createAction,
+} from "redux-dry-ts-actions"
 
 type State = {
-    tasks: Task[];
-    isRunning: boolean;
+    tasks: Task[]
+    isRunning: boolean
 }
 const defaultState: State = {
     tasks: defaultTasks,
-    isRunning: false
+    isRunning: false,
 }
 
 export const state = writable<State>(defaultState)
 
 type Events = {
-    taskTitleEdited: { index: number, title: string },
-    taskDeleted: { index: number },
-    taskStarted: void,
-    taskPaused: void,
+    taskTitleEdited: { index: number; title: string }
+    taskDeleted: { index: number }
+    taskStarted: void
+    taskPaused: void
 }
 
-type TODO = any;
+type TODO = any
 
 type Logic = {
     [eventName in keyof Events]: {
-        action: Events[eventName] extends Object ? ActionCreatorWithPayload<eventName, Events[eventName], TODO> : ActionCreator<eventName>;
-        updater: (payload: Events[eventName]) => (draft: WritableDraft<State>) => void;
+        action: Events[eventName] extends Object
+            ? ActionCreatorWithPayload<eventName, Events[eventName], TODO>
+            : ActionCreator<eventName>
+        updater: (payload: Events[eventName]) => (draft: WritableDraft<State>) => void
     }
 }
 
 export const logic: Logic = {
     taskTitleEdited: {
-        action: createAction('taskTitleEdited', (index, title) => ({ index, title })),
+        action: createAction("taskTitleEdited", (index, title) => ({ index, title })),
         updater: (payload) => (draft) => void (draft.tasks[payload.index].title = payload.title),
     },
     taskDeleted: {
-        action: createAction('taskDeleted', (index) => ({ index })),
-        updater: (payload) => (draft) => void draft.tasks.splice(payload.index, 1)
+        action: createAction("taskDeleted", (index) => ({ index })),
+        updater: (payload) => (draft) => void draft.tasks.splice(payload.index, 1),
     },
     taskStarted: {
-        action: createAction('taskStarted'),
-        updater: () => (draft) => void (draft.isRunning = true)
+        action: createAction("taskStarted"),
+        updater: () => (draft) => void (draft.isRunning = true),
     },
     taskPaused: {
-        action: createAction('taskPaused'),
-        updater: () => (draft) => void (draft.isRunning = false)
-    }
+        action: createAction("taskPaused"),
+        updater: () => (draft) => void (draft.isRunning = false),
+    },
 }
 
-
-export const actions = Object.fromEntries(Object.keys(logic).map(eventName => [eventName, logic[eventName].action]))
+export const actions = Object.fromEntries(Object.keys(logic).map((eventName) => [eventName, logic[eventName].action]))
 type Action = ActionTypeFromActionCreators<typeof actions>
 
 export function dispatch(action: Action) {
@@ -61,7 +67,7 @@ export function dispatch(action: Action) {
 }
 
 function handleAction(action: Action) {
-    const event = logic[action.type];
-    const updater = event.updater(action.payload);
+    const event = logic[action.type]
+    const updater = event.updater(action.payload)
     state.update((value) => produce(value, updater))
 }
