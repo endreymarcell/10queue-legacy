e9becb7ed646ca9de54c3aafc77aec97fa1f7d02
Marcell Endrey <marcell.endrey@prezi.com>
2021-10-10 19:47:10 +0200

    remove unused helper type

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,3 @@
-// Use this utility to force nominal typing, ie. to create a type
-// that is structurally equivalent to another one but still cannot be assigned to it
-export const uniqueTypeSymbol = Symbol.for("uniqueType")
-
 export function moveArrayElement<T>(array: ReadonlyArray<T>, fromIndex: number, toIndex: number): Array<T> {
     const result = [...array]
     const itemToAdd = result.splice(fromIndex, 1)[0]
