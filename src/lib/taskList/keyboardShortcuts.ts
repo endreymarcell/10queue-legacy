b3485fb05575e0921e7b2db81bf0484bbb5dccf1
Marcell Endrey <marcell.endrey@prezi.com>
2021-10-12 17:06:48 +0200

    add keyboard shortcuts for moving tasks up/down

--- src/lib/taskList/keyboardShortcuts.ts
+++ src/lib/taskList/keyboardShortcuts.ts
@@ -7,5 +7,7 @@ export function setupKeyboardShortcuts() {
         { key: "k", action: appLogic.taskActivatePreviousRequested.action, isDisabledDuringTextInput: true },
         { key: "d", action: appLogic.taskDeleteRequested.action, isDisabledDuringTextInput: true },
         { key: "c", action: appLogic.startedEditingTaskTitle.action, isDisabledDuringTextInput: true },
+        { key: "J", action: appLogic.taskMoveDownRequested.action, isDisabledDuringTextInput: true },
+        { key: "K", action: appLogic.taskMoveUpRequested.action, isDisabledDuringTextInput: true },
     ])
 }
