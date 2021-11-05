9076b7826dfd3f93f255daf6d42bdcc9cb748311
Marcell Endrey <marcell.endrey@prezi.com>
2021-11-05 22:46:30 +0100

    fix test setup

---
+++ src/routes/index.test.ts
@@ -0,0 +1,16 @@
+import { render, RenderResult } from "@testing-library/svelte"
+import Index from "./index.svelte"
+
+describe("Index", () => {
+    let renderedComponent: RenderResult
+
+    beforeEach(() => {
+        renderedComponent = render(Index)
+    })
+
+    describe("once the component has been rendered", () => {
+        test("should show the proper heading", () => {
+            expect(renderedComponent.getByText("Task queue")).toBeInTheDocument()
+        })
+    })
+})
